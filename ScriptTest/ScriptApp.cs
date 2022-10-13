2159bfcf534e70d55bf797ac1c171e8552758856
Dai Duong Nguyen <daiduong.nguyen@softwareone.com>
2022-10-13 11:44:08 +0200

    Add project files.

---
+++ ScriptTest/ScriptApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptTest
+{
+    public class ScriptApp
+    {
+        public ScriptApp(Form1 form1)
+        {
+            MainForm = form1;
+        }
+        public Form1 MainForm { get; init; }
+        public void AddLog(object message)
+        {
+            MainForm.txtResult.Text += $"{DateTime.Now:dd.MM.yyyy HH:mm:ss}: {message}\r\n";
+        }
+    }
+}
